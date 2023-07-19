ddbe4c463f620db3a60a01195096ef2aaa5900e8
gistomic <139983022+gistomic@users.noreply.github.com>
2023-07-19 21:08:38 +0700

    Add files via upload

---
+++ mian.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.write('# Menghitung luas hehew')
+st.write('by minum')
+
+panjang= st.number_input('Masukan luasnya', 0)
+lebar= st.number_input('Masukan Lebarnya', 0)
+Hitung_persegi= st.button('Hasil luas persegi')
+Hitung_keliling_persegi= st.button('hasil keliling pesegi')
+Hitung_luas_segitiga= st.button('hasil luas segitiga')
+
+if Hitung_persegi:
+    luas_persegi = panjang * lebar
+    st.success(f'Hasil luas persegi adalah:{luas_persegi}')
+if Hitung_keliling_persegi:
+    keliling_persegi = 2 * panjang + 2 * lebar
+    st.success(f'Hasil keliling persegi adalah:{keliling_persegi}')
+if Hitung_luas_segitiga:
+    luas_segitiga= 1/2 * panjang * lebar
+    st.success(f'Hasil luas segitiga adalah:{luas_segitiga}')
+
