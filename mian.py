ea0a7228cee9f4aa9a5895413cb630cc68e9c02e
gistomic <139983022+gistomic@users.noreply.github.com>
2023-07-19 22:04:36 +0700

    Update mian.py

--- mian.py
+++ mian.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.write('# Menghitung luas ;p')
+st.write('# Menghitung luas hehe')
 st.write('by minum')
 
 panjang= st.number_input('Masukan Panjangnya', 0)
@@ -10,10 +10,20 @@ Hitung_luas_segitiga= st.button('Hasil luas segitiga')
 
 if Hitung_persegi:
     luas_persegi = panjang * lebar
-    st.success(f'Hasil luas persegi adalah:{luas_persegi}')
+    st.success(f'Hasil luas persegi adalah:{luas_persegi}cm²')
 if Hitung_keliling_persegi:
     keliling_persegi = 2 * panjang + 2 * lebar
-    st.success(f'Hasil keliling persegi adalah:{keliling_persegi}')
+    st.success(f'Hasil keliling persegi adalah:{keliling_persegi}cm²')
 if Hitung_luas_segitiga:
     luas_segitiga= 1/2 * panjang * lebar
-    st.success(f'Hasil luas segitiga adalah:{luas_segitiga}')
+    st.success(f'Hasil luas segitiga adalah:{luas_segitiga}cm²')
+
+st.write('# idk if this website will be usefull or not :3')
+st.write('what 9+10')
+one= st.button('21')
+nine= st.button('19')
+
+if one:
+    st.success(':0')
+if nine:
+    st.success(":3")
